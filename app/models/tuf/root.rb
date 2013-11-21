a01798bcaaaf0287ec5c9a815c79ea3b15f3c6b7
Xavier Shay <xavier@squareup.com>
2013-11-21 14:57:21 -0800

    WIP

--- app/models/tuf/root.rb
+++ app/models/tuf/root.rb
@@ -1,5 +1,6 @@
 module Tuf
   # TODO: Justify this class' existence.
+  # Maybe it's actually a Keystore? Owner?
   class Root
     def initialize(content)
       # TODO: Justify use of unwrap_unsafe by documenting how this file is
@@ -20,8 +21,58 @@ module Tuf
       Tuf::Signer.unwrap(content, self)
     end
 
+    def to_hash
+      @root
+    end
+
+    def path_for(role)
+      role
+    end
+
     def fetch(key_id)
       key(key_id)
+    end
+
+    private
+
+    attr_reader :root
+
+    def key(key_id)
+      Tuf::Key.new(root['keys'].fetch(key_id))
+    end
+  end
+
+  # TODO: DRY this up with above
+  class Targets
+    def initialize(content)
+      # TODO: Can't use unwrap_unsafe here, need to unwrap with root
+      @target = content
+      @root = @target.fetch('delegations', {})
+    end
+
+    def sign_role(role, signer, content)
+      role_info = root['roles'].detect {|x| x['name'] == role }
+      role_info['keyids'].inject(signer.wrap(content)) do |content, key_id|
+        signer.sign(content, key(key_id))
+      end
+    end
+
+    def unwrap_role(role, content)
+      # TODO: get threshold for role rather than requiring all signatures to be
+      # valid.
+      Tuf::Signer.unwrap(content, self)
+    end
+
+    def to_hash
+      @target
+    end
+
+    def fetch(key_id)
+      key(key_id)
+    end
+
+    def path_for(role)
+      "targets/#{role}"
     end
 
     private

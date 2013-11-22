142be4b6a9702a68684e7f03cfa95f51110aa7a9
Xavier Shay <xavier@squareup.com>
2013-11-21 16:34:11 -0800

    Fix fetch-me-a-gem.

--- app/models/tuf/metadata_store.rb
+++ app/models/tuf/metadata_store.rb
@@ -23,50 +23,7 @@ module Tuf
     # TODO: This is now a weird name, since this "Repository" always operates
     # on the latest snapshot.
     def latest_snapshot
-      timestamp = bucket.get("metadata/timestamp.txt")
-      targets = if timestamp
-        signed_timestamp = JSON.parse(timestamp)
-        timestamp = root.unwrap_role('timestamp', signed_timestamp)
-
-        # TODO: Verify SHA in timestamp for release matches this file,
-        # otherwise vulnerable to freeze attack.
-        signed_release = JSON.parse(get_hashed_metadata("metadata/release.txt", timestamp['meta']).body)
-        release = root.unwrap_role('release', signed_release)
-
-        # TODO: Verify SHA in timestamp for release matches this file,
-        # otherwise vulnerable to freeze attack.
-        signed_targets = JSON.parse(get_hashed_metadata("metadata/targets.txt", release['meta']).body)
-        targets = root.unwrap_role('targets', signed_targets)
-
-        targets = Targets.new(targets)
-        # TODO: unclaimed should not be special, lazily fetch targets
-#         targets['delegations']['roles'].each do |delegated_role|
-#         end
-        signed_unclaimed = JSON.parse(get_hashed_metadata("metadata/targets/unclaimed.txt", release['meta']).body)
-        unclaimed = targets.unwrap_role('unclaimed', signed_unclaimed)
-
-        pp unclaimed
-        Tuf::Metadata.new(
-          unclaimed: unclaimed,
-          release:   release
-        )
-      else
-        raise "Needs bootstrap"
-      end
-    end
-
-    def get_hashed_target(path)
-      file = Tuf::File.from_metadata(path, targets['targets'][path])
-      file.attach_body! bucket.get(file.path_with_hash)
-    end
-
-    def get_target(path)
-      file = Tuf::File.from_metadata(path, targets['targets'][path])
-      file.attach_body! bucket.get(file.path)
-    end
-
-    def targets
-      @targets ||= begin
+      @latest_snapshot ||= begin
         timestamp = bucket.get("metadata/timestamp.txt")
         targets = if timestamp
           signed_timestamp = JSON.parse(timestamp)
@@ -80,11 +37,33 @@ module Tuf
           # TODO: Verify SHA in timestamp for release matches this file,
           # otherwise vulnerable to freeze attack.
           signed_targets = JSON.parse(get_hashed_metadata("metadata/targets.txt", release['meta']).body)
-          root.unwrap_role('targets', signed_targets)
+          targets = root.unwrap_role('targets', signed_targets)
+
+          targets = Targets.new(targets)
+          # TODO: unclaimed should not be special, lazily fetch targets
+  #         targets['delegations']['roles'].each do |delegated_role|
+  #         end
+          signed_unclaimed = JSON.parse(get_hashed_metadata("metadata/targets/unclaimed.txt", release['meta']).body)
+          unclaimed = targets.unwrap_role('unclaimed', signed_unclaimed)
+
+          Tuf::Metadata.new(
+            unclaimed: unclaimed,
+            release:   release
+          )
         else
-          {}
+          raise "Needs bootstrap"
         end
       end
+    end
+
+    def get_hashed_target(path)
+      file = Tuf::File.from_metadata(path, latest_snapshot.unclaimed['targets'][path])
+      file.attach_body! bucket.get(file.path_with_hash)
+    end
+
+    def get_target(path)
+      file = Tuf::File.from_metadata(path, latest_snapshot.unclaimed['targets'][path])
+      file.attach_body! bucket.get(file.path)
     end
 
     # Publishes new root, targets, and claimed roles that have been signed by
